fb81c5204a2087d9480ffb0c4f7ff00da3dd0f16
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-09-14 20:46:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms(attendees)
   room_messages
 end
 
-def printer
+def printer(attendees)
   badge_messages = batch_badge_creator(attendees)
   badge_messages.each do |message|
     puts message
